a16f08df9274707c5c61a27aa3b9eefd0dea7459
Diego Ulises Martínez Aguilar <diegoulisesmartinezaguilar@gmail.com>
2019-08-30 23:14:51 -0500

    Custom Types

--- platzi-dir/index.js
+++ platzi-dir/index.js
@@ -1,32 +1,29 @@
 'use strict'
 
-const { graphql, buildSchema } = require('graphql')
+const { buildSchema } = require('graphql')
 const express = require('express')
 const gqlMiddleware = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/resolvers')
 
 const app = express()
 const port = process.env.port || 3000
 
-// definiendo el esquema 
-const schema = buildSchema(`
-    type Query {
-        hello: String
-    }`
+// definiendo el esquema
+const schema = buildSchema(
+  readFileSync(
+    join(__dirname, 'lib', 'schema.graphql'),
+    'utf-8'
+  )
 )
 
-// configurar los resolvers
-const resolvers = {
-    hello: () => {
-        return 'Hola Mundo'
-    }
-}
-
 app.use('/api', gqlMiddleware({
-    schema: schema,
-    rootValue: resolvers,
-    graphiql: true
+  schema: schema,
+  rootValue: resolvers,
+  graphiql: true
 }))
 
 app.listen(port, () => {
-    console.log(`Server is listening at http://localhost:${port}/api`)
+  console.log(`Server is listening at http://localhost:${port}/api`)
 })
