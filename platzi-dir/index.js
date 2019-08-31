b0c709699ae711dbe31156c978dcb8c5663a6b7d
Diego Ulises Martínez Aguilar <diegoulisesmartinezaguilar@gmail.com>
2019-08-30 21:57:12 -0500

    Queries y Resolvers

--- platzi-dir/index.js
+++ platzi-dir/index.js
@@ -6,10 +6,21 @@ const { graphql, buildSchema } = require('graphql')
 const schema = buildSchema(`
     type Query {
         hello: String
+        saludo: String
     }`
 )
 
+// configurar los resolvers
+const resolvers = {
+    hello: () => {
+        return 'Hola Mundo'
+    },
+    saludo: () => {
+        return 'Hola a todos'
+    }
+}
+
 // ejecutar el query hello
-graphql(schema, '{ hello }').then((data) => {
+graphql(schema, '{ saludo }', resolvers).then((data) => {
     console.log(data)
 })
